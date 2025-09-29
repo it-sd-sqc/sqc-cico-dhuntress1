7f0f7d11194a974a8191c9c615215b28846ff337
Alan Ortiz <aortizcabrero@student.cvtc.edu>
2025-09-29 17:00:01 -0500

    Implement only digits be used as input

--- src/edu/cvtc/itsd/Main.java
+++ src/edu/cvtc/itsd/Main.java
@@ -37,11 +37,15 @@ public class Main {
   private static class InputFilter extends DocumentFilter {
     private static final int MAX_LENGTH = 8;
 
+    private static boolean onlyContainsNumbers(String str) {
+      return str.matches("\\d+");
+    }
+
     @Override
     public void insertString(FilterBypass fb, int offset, String stringToAdd, AttributeSet attr)
         throws BadLocationException
     {
-      if (fb.getDocument() != null) {
+      if (fb.getDocument() != null && (stringToAdd.isEmpty() || onlyContainsNumbers(stringToAdd))) {
         super.insertString(fb, offset, stringToAdd, attr);
       }
       else {
@@ -53,7 +57,7 @@ public class Main {
     public void replace(FilterBypass fb, int offset, int lengthToDelete, String stringToAdd, AttributeSet attr)
         throws BadLocationException
     {
-      if (fb.getDocument() != null) {
+      if (fb.getDocument() != null && (stringToAdd.isEmpty() || onlyContainsNumbers(stringToAdd))) {
         super.replace(fb, offset, lengthToDelete, stringToAdd, attr);
       }
       else {
